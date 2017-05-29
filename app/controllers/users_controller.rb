d5120d708bc961ef4725e2fbdef9a26d1b0030aa
Alex Henry <alexanderhenry@Alexs-MacBook-Pro.local>
2017-05-29 17:34:29 -0500

    Fixed nomethoderror and authorizationnotperformed issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   # https://github.com/CanCanCommunity/cancancan/wiki/authorizing-controller-actions
   # load_and_authorize_resource only: []
-  skip_authorization_check only: [:analytics_alias]
+  skip_authorization_check only: [:analytics_alias, :index]
 
   def analytics_alias
     # view file has JS that will identify the anonymous user through segment
     # after registration via "after devise registration path"
   end
 
-  def users
+  def index
     @users = User.all
   end
 end
